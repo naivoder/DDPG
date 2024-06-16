73fc5f4261669350e7ddbbad8f9b4ec72da447f6
Cameron Redovian <naivoder@gmail.com>
2024-06-16 17:55:49 -0400

    fixing animate.py errors

--- animate.py
+++ animate.py
@@ -1,32 +1,23 @@
 import gymnasium as gym
 import torch
-import numpy as np
-import imageio
 from agent import DDPGAgent
 from argparse import ArgumentParser
-import os
 from utils import save_animation
 
-def generate_animation(env_name, checkpoint_dir='checkpoints'):
+def generate_animation(env_name):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    env = gym.make_vec(env_name, num_envs=1)  # Single environment for rendering
-    agent = DDPGAgent(env.single_observation_space.shape, env.single_action_space.shape, tau=0.001)
+    env = gym.make(env_name, render_mode="rgb_array")
+    agent = DDPGAgent(env_name, env.observation_space.shape, env.action_space.shape, tau=0.001)
     agent.to(device)
     
-    # Load the latest checkpoint
-    checkpoint_path = os.path.join(checkpoint_dir, f'{env_name}_best.pth')
-    if os.path.exists(checkpoint_path):
-        agent.load_checkpoint(checkpoint_path)
-    else:
-        print(f"Checkpoint {checkpoint_path} not found.")
-        return
+    agent.load_checkpoints()
     
     frames = []
     states = env.reset()
     states = torch.tensor(states, dtype=torch.float32).to(device)
     term, trunc = False, False
     while not term and not trunc:
-        frames.append(env.render(mode="rgb_array"))
+        frames.append(env.render())
         actions = agent.choose_action(states)
         next_states, rewards, term, trunc, _ = env.step(actions)
         states = torch.tensor(next_states, dtype=torch.float32).to(device)
